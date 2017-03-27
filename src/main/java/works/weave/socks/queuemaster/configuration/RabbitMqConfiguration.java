337046301058a1b012ee3c985266f2920694b8b0
Roger Berlind <roger.berlind@apcera.com>
2017-03-27 14:37:53 -0400

    forgot to add import org.springframework.beans.factory.annotation.Value;

--- src/main/java/works/weave/socks/queuemaster/configuration/RabbitMqConfiguration.java
+++ src/main/java/works/weave/socks/queuemaster/configuration/RabbitMqConfiguration.java
@@ -7,6 +7,7 @@ import org.springframework.amqp.rabbit.core.RabbitAdmin;
 import org.springframework.amqp.support.converter.DefaultClassMapper;
 import org.springframework.amqp.support.converter.Jackson2JsonMessageConverter;
 import org.springframework.amqp.support.converter.MessageConverter;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import works.weave.socks.shipping.entities.Shipment;
